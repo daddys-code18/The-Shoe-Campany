2f71a92b09928c49d173d40513ebdeec38399fed
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2024-05-09 22:02:48 +0530

    about section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'media440': '440px',
+        'media560': '560px',
+        'media1200': '1200px'
+      },
+      backgroundImage: {
+        'testimonial-section': "url('/images/testimonial-section-background.png')",
+      }
+    },
   },
   plugins: [],
 }
